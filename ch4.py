0d351ad2aefa8614074fe1e2c8a116f541d6e4d8
Diyan Yordanov <diyordan@cisco.com>
2014-12-13 14:41:57 +0200

    Serialize and deserialize python object
    Signed-off-by: Diyan Yordanov <diyordan@cisco.com>

--- ch4.py
+++ ch4.py
@@ -19,3 +19,11 @@ try:
 		print("It's...", file=data)
 except IOError as err:
 	print('File error: ' + str(err))
+
+''' Store obj to and load obj from persistence'''
+import pickle 
+with open('mydata.pickle', 'wb') as mysavedata:
+	pickle.dump([1, 2, 'three'], mysavedata)
+with open('mydata.pickle', 'rb') as myrestoredata:
+	a_list = pickle.load(myrestoredata)
+print(a_list)
