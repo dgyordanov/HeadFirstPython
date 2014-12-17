d07225a5a284dd634a1cf978cb99f843c0e2254a
Diyan Yordanov <diyordan@cisco.com>
2014-12-17 10:19:11 +0200

    Add list comprehensions and set manipulations
    Signed-off-by: Diyan Yordanov <diyordan@cisco.com>

---
+++ ch5.py
@@ -0,0 +1,13 @@
+# -- List comprehensions
+mins = [1, 2, 3, 4, 5, 6]
+# Convert mins to secs for each list item
+secs = [m * 60 for m in mins]
+print(secs)
+
+
+# create set
+distances = {10.6, 11, 8, 10.6, "two", 7}
+print(distances)
+# or
+distances = set([10.6, 11, 8, 10.6, "two", 7])
+print(distances)
